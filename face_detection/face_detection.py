6dbfdca7b7dfba75fbf755a6f60850e0697f84e4
joji <jojijoseph@iisc.ac.in>
2021-10-10 23:55:53 +0530

    Fix minor bug in face_detection.py

--- face_detection/face_detection.py
+++ face_detection/face_detection.py
@@ -5,7 +5,7 @@ import numpy as np
 
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
-q
+
 cap = cv2.VideoCapture(0)
 
 while True:
